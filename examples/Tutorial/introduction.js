2b8e4b2c96db3f5711bdcc1ab86774168c32ddba
nhanaswigs <nhan.aswigs@gmail.com>
2014-05-02 18:49:42 +0700

    Add tutorial folder
    Not yet finish but kind of basic tutorial

---
+++ examples/Tutorial/introduction.js
@@ -0,0 +1,35 @@
+// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
+function AppViewModel() {
+    var self = this;
+    this.firstName = html.data("Bert");
+    this.lastName = html.data("Bertington");
+
+    this.fullName = html.data(function() {
+        return self.firstName() + " " + self.lastName();    
+    });
+
+    this.capitalizeLastName = function() {
+        var currentVal = self.lastName();        // Read the current value
+        self.lastName(currentVal.toUpperCase()); // Write back a modified value
+    };    
+}
+
+var vm = new AppViewModel();
+
+html.render(document.body)
+    .div()
+        .span('First name: ').$().span(vm.firstName).$().br()
+        .span('Last name: ').$().span(vm.lastName).$()
+    .$()
+    .div()
+        .span('First name: ').$().input(vm.firstName).f5(vm).$().br()
+        .span('Last name: ').$().input(vm.lastName).f5(vm).$()
+    .$()
+    .div()
+        .span('Full name: ').$().span(vm.fullName).$()
+    .$()
+    .button('Capitalize last name').click(vm.capitalizeLastName).$()
+    
+    
+    
+    
